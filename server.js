00f22ee4564bc293aae78a62585d08b9a333d0cc
saddamhr <saddambubt65@gmail.com>
2020-09-06 11:37:11 +0600

    run reactjs and nodejs in same terminal

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 // HTTP request logger
 app.use(morgan('tiny'));
