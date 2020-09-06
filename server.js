28e40158ca8a9265f240ee034faecba9c31ecca7
saddamhr <saddambubt65@gmail.com>
2020-09-06 10:59:56 +0600

    setup the reactjs and nodejs structure

---
+++ server.js
@@ -0,0 +1,30 @@
+// Import npm packages
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// HTTP request logger
+app.use(morgan('tiny'));
+
+// Routes
+app.get('/api', (req, res) => {
+    const data = {
+        username: 'Saddam',
+        age: 5
+    };
+    res.json(data);
+});
+
+app.get('/api/name', (req, res) => {
+    const data = {
+        username: 'Saddam',
+        age: 5
+    };
+    res.json(data);
+});
+
+app.listen(PORT, console.log(`Server is starting at ${PORT}`));
